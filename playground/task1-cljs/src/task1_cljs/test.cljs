27f2418904533ebfb352f133390e01722b5f556d
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-06-24 14:17:34 +0300

    first async test

--- playground/task1-cljs/src/task1_cljs/test.cljs
+++ playground/task1-cljs/src/task1_cljs/test.cljs
@@ -1,14 +1,40 @@
 (ns task1-cljs.test
-  (:require-macros [cljs.test :refer (is deftest run-tests)])
+  (:require-macros [cljs.test :refer [is deftest run-tests]]
+                   [task1-cljs.macros :refer [node-require]]
+                   [cljs.core.async.macros :refer [go]])
+
   (:require [cljs.nodejs :as node]
-            [cljs.test :as t]))
+            [cljs.core.async :as async :refer [<! chan take!]]
+            [cljs.test :as t]
+            [task1-cljs.cljs-callbacks :as cb]))
+
+(node-require [fs])
+
+(def original-readFile (.-readFile fs))
 
 
 (node/enable-util-print!)
 
 
-(deftest arithmetics
-  (is (= (* 2 2) 5)))
+(defn- readFile-mock [name callback]
+  (go
+    (<! (async/timeout 30))
+    (callback nil name)))
+
+
+(t/use-fixtures :each
+  {:before #(set! (.-readFile fs) readFile-mock)
+   :after  #(set! (.-readFile fs) original-readFile)})
+
+
+(deftest cljs-callbacks
+  (t/async done
+           (let [callback (fn [err data]
+                            (is (nil? err))
+                            (is (= 6 data))
+                            (done))]
+
+             (cb/sum-from-files ["1" "2" "3"] callback))))
 
 
 (defn -main []

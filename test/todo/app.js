824b3db062b0bb46fa7f65ac2a1f38e9b09d531e
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-07-27 16:47:51 +0300

    simple implementation to pass a test

--- test/todo/app.js
+++ test/todo/app.js
@@ -6,13 +6,13 @@ const {TodoApp} = require("imp/todo");
 describe("TodoApp", ()=> {
     it("should allow to create user and add several todo items", () => {
         const app = TodoApp.new();
-        app.new();
+        app.start();
         Promise.coroutine(function* () {
             yield app.start();
             const aliceId = yield app.createUser("Alice");
             yield app.addTodo(aliceId, "Feed the cat");
             yield app.addTodo(aliceId, "Wash the dog");
-            const todos = app.listTodos(aliceId);
+            const todos = yield app.listTodos(aliceId);
             assert.equal(todos.length, 2);
         })();
     });

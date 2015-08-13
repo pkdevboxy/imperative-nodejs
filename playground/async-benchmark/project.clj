5b923c4e66ad2ce77209082304407ffb483b5e1f
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-08-13 20:14:44 +0300

    bump deps

--- playground/async-benchmark/project.clj
+++ playground/async-benchmark/project.clj
@@ -7,7 +7,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [funcool/promesa "0.1.3"]
                  [clj-http "1.1.2"]
